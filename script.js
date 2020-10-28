9c5301d6a56d36ec3435c29d151cfa2a7b845732
Richard Shaw <richardmorganshaw@gmail.com>
2020-10-28 11:08:15 -0700

    Added Math

---
+++ script.js
@@ -0,0 +1,31 @@
+const modulo = (n1, n2) => {
+  return n1 % n2
+}
+
+const divide = (n1, n2) => {
+  return n1 / n2
+}
+
+const multiply = (n1, n2) => {
+  return n1 * n2;
+}
+
+const subtract = (n1, n2) => {
+  return n1 - n2;
+}
+
+const addition = (n1, n2) => {
+  return n1 + n2;
+}
+
+//Tests
+
+console.log(addition(10, 15));
+
+console.log(subtract(12, 14));
+
+console.log(multiply(4, 4));
+
+console.log(divide(12, 3));
+
+console.log(modulo(9, 6));
